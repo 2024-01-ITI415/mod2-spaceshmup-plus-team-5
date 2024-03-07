2b7fdfe49e9bdc8463b7aa0312b74e6069656c39
Hamza <79418669+HA-Hamza-GITHUB@users.noreply.github.com>
2024-03-07 16:59:10 -0500

    Updating ResetButton
    Finding out issues

--- Assets/__Scripts/ResetButton.cs
+++ Assets/__Scripts/ResetButton.cs
@@ -3,8 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class ResetButton : MonoBehaviour
 {
-    public void PlayAgain()
+    // This function will be called when the button is clicked
+    public void ResetGame()
     {
+        // Add any logic you need before resetting the game
+        Debug.Log("Game Reset!");
         SceneManager.LoadScene("_Scene_0");
     }
 }
