1a139b58c63a81cf2ff1d2888a35d5236c8f26f0
BromieMiGui <157049782+BromieMiGui@users.noreply.github.com>
2024-03-07 19:43:04 -0500

    delosReyes_IanMiguel_1.6
    -Enemy Projectile Changes

--- Assets/__Scripts/Projectile_Enemy.cs
+++ Assets/__Scripts/Projectile_Enemy.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class Projectile_Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private BoundsCheck bndCheck;
+    private Renderer rend;
+
+    [Header("Set Dynamically")]
+    public Rigidbody rigid;
+    [SerializeField]
+    private WeaponType _type;
+
+    // This public property masks the field _type and takes action when it is set
+    public WeaponType type
     {
-        
+        get
+        {
+            return (_type);
+        }
+        set
+        {
+            SetType(value);
+        }
+    }
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+        rend = GetComponent<Renderer>();
+        rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (bndCheck.offUp)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    ///<summary>
+    /// Sets the _type private field and colors this projectile to match the
+    /// WeaponDefinition.
+    /// </summary>
+    /// <param name="eType">The WeaponType to use.</param>
+    public void SetType(WeaponType eType)
+    {
+        // Set the _type
+        _type = eType;
+        WeaponDefinition def = Main.GetWeaponDefinition(_type);
+        rend.material.color = def.projectileColor;
     }
 }
