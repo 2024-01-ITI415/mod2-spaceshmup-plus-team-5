452f3a5c2f07b8c993e7cd9427aa756dfe31ac2f
Hamza <79418669+HA-Hamza-GITHUB@users.noreply.github.com>
2024-03-08 22:35:31 -0500

    Changing Scripts
    Need to do this in order to revert

--- Assets/__Scripts/HighscoreManager.cs
+++ Assets/__Scripts/HighscoreManager.cs
@@ -1,3 +1,4 @@
+/*
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -116,4 +117,163 @@ public class HighscoreManager : MonoBehaviour
         Debug.Log("Loaded Highscores: " + string.Join(", ", highscores));
     }
 
+}*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HighscoreManager : MonoBehaviour
+{
+    public static HighscoreManager instance;
+
+    public int[] highscores = new int[5]; // Top 5 highscores
+    public Text highscoreText;
+    
+    // Add a new variable to track the scene index
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        // Load highscores first
+        LoadHighscores();
+
+        // Set the initial value for highScore
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+
+        // Create empty highscore slots of zeroes if needed
+        for (int i = 0; i < highscores.Length; i++)
+        {
+            if (highscores[i] == 0)
+            {
+                highscores[i] = PlayerPrefs.GetInt($"HighScore{i}", 0);
+            }
+        }
+
+        UpdateHighscoreUI();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadHighscores();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // Initialize highscores on startup
+        for (int i = 0; i < highscores.Length; i++)
+        {
+            // Use unique keys for each highscore entry
+            string key = $"HighScore{i}";
+            if (PlayerPrefs.HasKey(key))
+            {
+                highscores[i] = PlayerPrefs.GetInt(key);
+            }
+            else
+            {
+                highscores[i] = 0;
+                PlayerPrefs.SetInt(key, 0);
+            }
+        }
+
+        UpdateHighscoreUI();
+    }
+
+    // Add a new method to set the current scene index
+    public void SetCurrentSceneIndex(int index)
+    {
+        currentSceneIndex = index;
+    }
+
+    // Add a new method to check if the current scene is the PostGameMenuScene
+    public bool IsPostGameMenuScene()
+    {
+        return SceneManager.GetActiveScene().name == "PostGameMenuScene";
+    }
+
+    // Modify the AddHighscore method to update UI only in the PostGameMenuScene
+    public void AddHighscore(int score)
+    {
+        for (int i = 0; i < highscores.Length; i++)
+        {
+            if (score > highscores[i])
+            {
+                for (int j = highscores.Length - 1; j > i; j--)
+                {
+                    highscores[j] = highscores[j - 1];
+                }
+                highscores[i] = score;
+
+                // Print highscores for debugging
+                Debug.Log("Updated Highscores: " + string.Join(", ", highscores));
+
+                // Update UI only in the PostGameMenuScene
+                if (IsPostGameMenuScene())
+                {
+                    UpdateHighscoreUI();
+                }
+
+                break;
+            }
+        }
+
+        // Save highscores only if not in the PostGameMenuScene
+        if (!IsPostGameMenuScene())
+        {
+            SaveHighscores();
+        }
+    }
+
+    // Modify the UpdateHighscoreUI method to update UI only in the PostGameMenuScene
+    private void UpdateHighscoreUI()
+    {
+        if (IsPostGameMenuScene())
+        {
+            string displayText = "Highscores:\n";
+
+            for (int i = 0; i < 5; i++)
+            {
+                int score = highscores[i];
+                displayText += $"{i + 1}. {score}\n";
+            }
+
+            // Print retrieved highscores for debugging
+            Debug.Log("Retrieved Highscores: " + displayText);
+
+            highscoreText.text = displayText;
+        }
+    }
+
+    // Modify the SaveHighscores method to save only if not in the PostGameMenuScene
+    private void SaveHighscores()
+    {
+        if (!IsPostGameMenuScene())
+        {
+            // Save highscores
+            for (int i = 0; i < highscores.Length; i++)
+            {
+                string key = $"HighScore{i}";
+                PlayerPrefs.SetInt(key, highscores[i]);
+            }
+            PlayerPrefs.Save(); // Save changes
+        }
+    }
+
+    public void LoadHighscores()
+    {
+        for (int i = 0; i < highscores.Length; i++)
+        {
+            highscores[i] = PlayerPrefs.GetInt($"HighScore{i}", 0);
+        }
+
+        // Print loaded highscores for debugging
+        Debug.Log("Loaded Highscores: " + string.Join(", ", highscores));
+    }
 }
+
--- Assets/__Scripts/PostGameMenu.cs
+++ Assets/__Scripts/PostGameMenu.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.UI;
 
 public class PostGameMenu : MonoBehaviour
@@ -40,6 +40,53 @@ public class PostGameMenu : MonoBehaviour
 
         highscoreText.text = displayText;
 
+    }
+}*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PostGameMenu : MonoBehaviour
+{
+    public Text highscoreText;
+
+    void Start()
+    {
+        Debug.Log($"HighScore0: {PlayerPrefs.GetInt("HighScore0", 0)}");
+        Debug.Log($"HighScore1: {PlayerPrefs.GetInt("HighScore1", 0)}");
+        // ... Repeat for HighScore2, HighScore3, HighScore4
+
+        // Load highscores from HighscoreManager
+        HighscoreManager.instance.LoadHighscores();
+
+        // Retrieve the current highscore from PlayerPrefs
+        int currentHighscore = PlayerPrefs.GetInt("CurrentHighscore", 0);
+
+        // Display the current highscore or use it as needed
+        Debug.Log($"Current Highscore: {currentHighscore}");
+
+        // Update the UI with highscores only in the PostGameMenuScene
+        if (HighscoreManager.instance.IsPostGameMenuScene())
+        {
+            UpdateHighscoreUI();
+        }
+    }
+
+    // Modify the UpdateHighscoreUI method to update UI only in the PostGameMenuScene
+    void UpdateHighscoreUI()
+    {
+        string displayText = "Highscores:\n";
+
+        for (int i = 0; i < 5; i++)
+        {
+            int score = HighscoreManager.instance.highscores[i];
+            displayText += $"{i + 1}. {score}\n";
+        }
+
+        // Print retrieved highscores for debugging
+        Debug.Log("Retrieved Highscores: " + displayText);
+
+        highscoreText.text = displayText;
     }
 }
 
